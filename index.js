3cf4a5e82e875f147b1c40b5af9daf3189fcda56
Hilary Ehlers <hilarydixson@gmail.com>
2024-05-25 22:30:49 -0500

    arithmetic review lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16;
+let num4 = 6;
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(10, 5, 20, 15);
+console.log(max);
